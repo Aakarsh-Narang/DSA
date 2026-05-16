5bc9b5b5ce4a3f0b01b6d40817917db396c8781b
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-05-16 16:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (39.31%) - LeetSync

--- 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.cpp
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    static bool compare(vector<int>& a, vector<int>& b){
-        if(a[1] == b[1]) return a[0] > b[0];
-        return a[1] < b[1];
+    static bool cmp(vector<int>& a, vector<int>& b){
+        if(a[0] == b[0]) return a[1] > b[1];
+        return a[0] < b[0];
     }
     int intersectionSizeTwo(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), compare);
-
-        // for(auto& v:intervals){
-        //     cout<< v[0] << " "<< v[1] << endl;
-        // }
-        // last2 < last1 (assumption for the algo)
-        int n = intervals.size(), last1 = intervals[0][1], last2 = intervals[0][1]-1;
-        // We will also build nums vector for a more versatile soln
-        vector<int> nums;
-        nums.push_back(last2);
-        nums.push_back(last1);
-        for(int i = 1; i<n; i++){
-            // Current interval is already satisfied
-            if(last1 >= intervals[i][0] && last2 >= intervals[i][0]) continue;
-            if(last1 >= intervals[i][0] || last2 >= intervals[i][0]){
-                last2 = last1;
-                last1 = intervals[i][1];
-                nums.push_back(last1);
+        sort(intervals.begin(), intervals.end(), cmp);
+        vector<int> ans;
+        int n = intervals.size();
+        int last1 = intervals[n-1][0]+1, last2 = intervals[n-1][0];
+        ans.push_back(last1);
+        ans.push_back(last2);
+        for(int i = n-2; i >= 0; i--){
+            int end = intervals[i][1];
+            if(last1 <= end && last2 <= end) continue;
+            else if(last1 <= end || last2 <= end){
+                last1 = last2;
+                last2 = intervals[i][0];
+                ans.push_back(last1);
             }
             else{
-                last1 = intervals[i][1];
-                last2 = intervals[i][1]-1;
-                nums.push_back(last2);
-                nums.push_back(last1);
+                last2 = intervals[i][0];
+                last1 = intervals[i][0]+1;
+                ans.push_back(last1);
+                ans.push_back(last2);
             }
         }
-        // for(auto& n:nums) cout<<n<<" ";
-        return nums.size();
+        return ans.size();
     }
 };
