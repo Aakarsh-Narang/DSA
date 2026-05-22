7ce8962880e37a84de89ff9ddc5ac471bd646dcb
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-05-22 11:48:33 +0530

    Time: 29 ms (35.58%) | Memory: 139.1 MB (66.52%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -5,14 +5,14 @@ public:
         vector<int> ans;
         int n = nums.size();
         for(int i = 0; i < n; i++){
+            // Pop expired or out of window elements from front
+            while(!dq.empty() && dq.front() <= i-k)
+                dq.pop_front();
+                
             // Maintain monotonicity: From back remove all smaller elements
             while(!dq.empty() && nums[dq.back()] <= nums[i])
                 dq.pop_back();
             dq.push_back(i);
-
-            // Pop expired or out of window elements from front
-            while(!dq.empty() && dq.front() <= i-k)
-                dq.pop_front();
 
             // Build Answer Array
             if(i >= k-1)
