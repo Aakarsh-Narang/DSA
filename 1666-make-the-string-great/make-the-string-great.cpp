18e2da177c2a49ef0dc1decbc3015b604e9e0ea2
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-05-23 11:16:33 +0530

    Time: 1 ms (24.31%) | Memory: 8.9 MB (22.84%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     string makeGood(string s) {
-        string ans ="";
-        bool flg = 1;
-        while(flg){
-            flg = 0;
-            int n = s.size();
-            for(int i = 0; i<n; i++){
-                if(i+1 < n && abs(s[i] - s[i+1]) == 32){
-                    i++;
-                    flg = 1;
-                }
-                else{
-                    ans += s[i];
-                }
+        stack<char> st;
+        int n = s.size();
+        for(int i = 0; i<n; i++){
+            while(!st.empty() && i<n && abs(s[i] - st.top()) == 32){
+                st.pop();
+                i++;
             }
-            s = ans;
-            ans = "";
+            if(i<n)
+                st.push(s[i]);
         }
-        return s;
+        string ans="";
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
