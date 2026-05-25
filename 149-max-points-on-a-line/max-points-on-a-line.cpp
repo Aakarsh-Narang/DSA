7301d4d9ce05fad01ca90d17c40c8629fc10b7c6
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-05-25 14:22:13 +0530

    Time: 38 ms (33.44%) | Memory: 23.2 MB (16.76%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
+    struct HashPair {
+        size_t operator()(const pair<int, int>& p) const{
+            return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+        }
+    };
     int maxPoints(vector<vector<int>>& points) {
         int ans = 1, n = points.size();
         for(int i = 0; i < n; i++){
-            unordered_map<double, int> mp;
-            for(int j = i+1; j < n; j++){
+            unordered_map<pair<int, int>, int, HashPair> mp;
+            for(int j = 0; j < n; j++){
+                if(i == j) continue;
                 int dx = points[i][0] - points[j][0];
                 int dy = points[i][1] - points[j][1];
-                // int g = gcd(dx, dy);
-                // // Normalizing
-                // dx /= g;
-                // dy /= g;
-
-                double slope;
-                if(dx == 0) slope = 1e8;
-                else slope = (1.0 * dy) / dx;
+                int g = gcd(dx, dy);
+                dx /= g;
+                dy /= g;
+                pair<int, int> slope = make_pair(dx, dy);
                 mp[slope]++;
                 ans = max(ans, mp[slope]+1);
             }
