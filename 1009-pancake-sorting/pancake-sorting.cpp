82acadcac68b91a55b9e24e715f55da2267d71d1
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-05-25 21:11:10 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (90.60%) - LeetSync

---
+++ 1009-pancake-sorting/pancake-sorting.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void printArr(vector<int>& arr){
+        for(auto&  e: arr)
+            cout<<e << " ";
+        cout<<endl;
+    }
+    vector<int> pancakeSort(vector<int>& arr) {
+        vector<int> ans;
+        int n = arr.size();
+        for(int i = 0; i < n; i++){
+            int target = n-i;
+            int ff;  // First-Flip
+            for(int j = 0; j < n; j++){
+                if(arr[j] == target){
+                    ff = j;
+                    break;
+                }
+            }
+            if(ff == target-1 ) continue;
+
+            ans.push_back(ff+1);
+            ans.push_back(target);
+
+            reverse(arr.begin(), arr.begin() + ff+1);
+            // printArr(arr);
+            
+            reverse(arr.begin(), arr.begin() + target);
+            // printArr(arr);
+        }
+        return ans;
+    }
+};
