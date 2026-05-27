5a8a6665e43c3b3ba43dc027ac46b0b28ba32d0d
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-05-27 20:28:25 +0530

    Time: 1 ms (93.21%) | Memory: 12 MB (89.75%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -1,75 +1,43 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<string> st;
-        int n = s.size();
-        for(int i = 0; i < n; i++){
-            // Ignore spaces
-            if(s[i] == ' ') continue;
+        int n = s.size(), ans = 0, idx = 0, prev;
 
-            // Parse complete number
-            if(isdigit(s[i])){
-                string num = "";
+        while(idx < n && s[idx] == ' ') idx++;
+        while(idx < n && isdigit(s[idx])) {
+            ans = ans * 10 + (s[idx++] -'0');
+        }
+        cout<< ans <<" ";
+        prev = ans;
+        for(int i = idx; i < n; ){
+            int curr = 0;
+            while(i < n && s[i] == ' ') i++;
+            if(i >= n) break;
 
-                while(i < n && isdigit(s[i])){
-                    num += s[i];
-                    i++;
-                }
-                i--; // compensate extra increment
-                st.push(num);
+            char op = s[i++];
+
+            while(i < n && s[i] == ' ') i++;
+            if(i >= n) break;
+
+            while(i < n && isdigit(s[i])) {
+                curr = curr * 10 + (s[i++] -'0');
             }
-
-            else if(s[i] == '+' || s[i] == '-'){
-                st.push(string(1, s[i]));
+            if(op == '+'){
+                ans = ans + curr;
+                prev = curr;
             }
-
+            else if(op == '-'){
+                ans = ans - curr;
+                prev = -curr;
+            }
+            else if(op == '*'){
+                ans = ans - prev + (prev * curr);
+                prev = prev * curr;
+            }
             else{
-                 // Get next complete number
-                string num = "";
-                i++;
-
-                while(i < n && s[i] == ' ')
-                    i++;
-
-                while(i < n && isdigit(s[i])){
-                    num += s[i];
-                    i++;
-                }
-                i--;
-                int right = stoi(num);
-                int left = stoi(st.top());
-
-                st.pop();
-                int val;
-
-                if(s[i - num.size()] == '*'){
-                    val = left * right;
-                }
-                else {
-                    val = left / right;
-                }
-                st.push(to_string(val));
+                ans = ans - prev + (prev / curr);
+                prev = prev / curr;
             }
-        }
-
-        // Evaluate remaining + and -
-        vector<string> expr;
-
-        while(!st.empty()){
-            expr.push_back(st.top());
-            st.pop();
-        }
-
-        reverse(expr.begin(), expr.end());
-        int ans = stoi(expr[0]);
-
-        for(int i = 1; i < expr.size(); i += 2){
-            string op = expr[i];
-            int num = stoi(expr[i + 1]);
-            if(op == "+")
-                ans += num;
-            else
-                ans -= num;
         }
         return ans;
     }
