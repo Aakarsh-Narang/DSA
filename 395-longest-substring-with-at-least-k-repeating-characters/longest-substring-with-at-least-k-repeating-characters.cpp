b22c1f807ccc79eebbcdacd1f9419f3fb051bea6
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-05-31 16:23:08 +0530

    Time: 11 ms (57.78%) | Memory: 9.2 MB (67.04%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     int longestSubstring(string s, int k) {
-        int uniqueCount, ans = 0, l = 0, r = 0, n = s.size();
-
+        int n = s.size(), ans = 0;
+        // No of unique chars allowed in the window
         for(int maxUnique = 1; maxUnique <= 26; maxUnique++){
             unordered_map<char, int> mp;
-            l = 0, r = 0, uniqueCount = 0;
+            int satisfiedCnt = 0, l = 0, r = 0;
             while(l < n && r < n){
                 mp[s[r]]++;
                 if(mp[s[r]] == k){
-                    uniqueCount++;
+                    satisfiedCnt++;
                 }
 
-                if(uniqueCount == mp.size())
-                    ans = max(ans, r-l+1);
-
-                while(l <= r && mp.size() > maxUnique){
-                    if(mp[s[l]] == k) uniqueCount--;
+                while(mp.size() > maxUnique && l <= r){
+                    // Check Unique
+                    if(mp[s[l]] == k){
+                        satisfiedCnt--;
+                    }
                     mp[s[l]]--;
+                    // Check if last one in Window
                     if(mp[s[l]] == 0){
                         mp.erase(s[l]);
                     }
                     l++;
                 }
+
+                // Ans condition satisfied
+                if(satisfiedCnt == mp.size()){
+                    ans = max(ans, r - l + 1);
+                }
                 r++;
             }
         }
-
         return ans;
     }
 };
