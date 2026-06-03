dbc2ea586f2e119eeb23293fcc1d890ea6719351
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-04 00:31:52 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (62.74%) - LeetSync

---
+++ 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+        int minSoFar = 1e8, maxSoFar = 0, minIndx = 0, maxIndx = 0;
+        int l = 0, r = indexDifference, n = nums.size();
+
+        while(r < n){
+            if(nums[l] < minSoFar){
+                minIndx = l;
+                minSoFar = nums[l];
+            }
+            if(nums[l] > maxSoFar){
+                maxIndx = l;
+                maxSoFar = nums[l];
+            }
+
+            if(abs(nums[r] - minSoFar) >= valueDifference){
+                return {minIndx,r};
+            }
+            if(abs(nums[r] - maxSoFar) >= valueDifference){
+                return {maxIndx, r};
+            }
+
+            l++;
+            r++;
+        }
+
+        return {-1,-1};
+    }
+};
