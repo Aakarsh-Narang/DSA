3d69623cab745bc4b886aa5534229420c161c134
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-03 16:37:23 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (76.48%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        for(int i = 0; i < n; i++){
+            if(citations[n-i-1] < (i+1)) 
+                return i;
+        }
+        return n;
+    }
+};
