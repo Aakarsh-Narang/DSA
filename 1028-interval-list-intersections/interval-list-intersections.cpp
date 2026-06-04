1b18d326d5dbd9171b81bcab9acbc5d57732b946
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-04 12:25:39 +0530

    Time: 4 ms (30.97%) | Memory: 22.9 MB (85.20%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -9,9 +9,8 @@ public:
             int s = max(firstList[i][0], secondList[j][0]);
             int e = min(firstList[i][1], secondList[j][1]);
 
-            if(s <= e){
+            if(s <= e)
                 ans.push_back({s, e});
-            }
             
             if(firstList[i][1] < secondList[j][1]) 
                 i++;
