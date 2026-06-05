4369f1f02091ef94cbf5302e1a946e08e9a5b48e
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-05 17:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 26.7 MB (39.73%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> next(n, 0), prev(n, 0);
+        vector<int> prev(n, 0);
 
         int maxi = 0;
         for(int i = 0; i < n; i++){
