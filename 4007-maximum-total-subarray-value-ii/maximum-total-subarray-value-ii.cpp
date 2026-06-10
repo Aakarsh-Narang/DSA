19c6b0da9bb6f738fb4835e04c59b1e882ee163d
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-10 20:28:06 +0530

    Time: 459 ms (81.45%) | Memory: 154.1 MB (79.03%) - LeetSync

--- 4007-maximum-total-subarray-value-ii/maximum-total-subarray-value-ii.cpp
+++ 4007-maximum-total-subarray-value-ii/maximum-total-subarray-value-ii.cpp
@@ -1,32 +1,66 @@
+class SegTree {
+    vector<int> maxv, minv;
+    int n;
+
+public:
+    SegTree(vector<int>& nums) {
+        n = nums.size();
+        maxv.resize(n * 4);
+        minv.resize(n * 4);
+        build(1, 0, n - 1, nums);
+    }
+
+    void build(int node, int l, int r, vector<int>& nums) {
+        if (l == r) {
+            maxv[node] = minv[node] = nums[l];
+            return;
+        }
+        int m = (l + r) / 2;
+        build(node * 2, l, m, nums);
+        build(node * 2 + 1, m + 1, r, nums);
+        maxv[node] = max(maxv[node * 2], maxv[node * 2 + 1]);
+        minv[node] = min(minv[node * 2], minv[node * 2 + 1]);
+    }
+
+    int queryMax(int node, int l, int r, int ql, int qr) {
+        if (ql <= l && r <= qr) {
+            return maxv[node];
+        }
+        int m = (l + r) / 2, res = INT_MIN;
+        if (ql <= m) {
+            res = max(res, queryMax(node * 2, l, m, ql, qr));
+        }
+        if (qr > m) {
+            res = max(res, queryMax(node * 2 + 1, m + 1, r, ql, qr));
+        }
+        return res;
+    }
+
+    int queryMin(int node, int l, int r, int ql, int qr) {
+        if (ql <= l && r <= qr) {
+            return minv[node];
+        }
+        int m = (l + r) / 2, res = INT_MAX;
+        if (ql <= m) {
+            res = min(res, queryMin(node * 2, l, m, ql, qr));
+        }
+        if (qr > m) {
+            res = min(res, queryMin(node * 2 + 1, m + 1, r, ql, qr));
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
         int n = nums.size();
-        int logn = 32 - __builtin_clz(n);
-        vector<vector<int>> stMax(n, vector<int>(logn));
-        vector<vector<int>> stMin(n, vector<int>(logn));
-        for (int i = 0; i < n; i++) {
-            stMax[i][0] = stMin[i][0] = nums[i];
-        }
-        for (int j = 1; j < logn; j++) {
-            for (int i = 0; i + (1 << j) <= n; i++) {
-                stMax[i][j] =
-                    max(stMax[i][j - 1], stMax[i + (1 << (j - 1))][j - 1]);
-                stMin[i][j] =
-                    min(stMin[i][j - 1], stMin[i + (1 << (j - 1))][j - 1]);
-            }
-        }
-        auto queryMax = [&](int l, int r) {
-            int j = 31 - __builtin_clz(r - l + 1);
-            return max(stMax[l][j], stMax[r - (1 << j) + 1][j]);
-        };
-        auto queryMin = [&](int l, int r) {
-            int j = 31 - __builtin_clz(r - l + 1);
-            return min(stMin[l][j], stMin[r - (1 << j) + 1][j]);
-        };
+        SegTree seg(nums);
         priority_queue<tuple<int, int, int>> pq;
         for (int l = 0; l < n; l++) {
-            pq.emplace(queryMax(l, n - 1) - queryMin(l, n - 1), l, n - 1);
+            pq.emplace(seg.queryMax(1, 0, n - 1, l, n - 1) -
+                           seg.queryMin(1, 0, n - 1, l, n - 1),
+                       l, n - 1);
         }
         long long ans = 0;
         while (k--) {
@@ -34,7 +68,9 @@ public:
             pq.pop();
             ans += val;
             if (r > l) {
-                pq.emplace(queryMax(l, r - 1) - queryMin(l, r - 1), l, r - 1);
+                pq.emplace(seg.queryMax(1, 0, n - 1, l, r - 1) -
+                               seg.queryMin(1, 0, n - 1, l, r - 1),
+                           l, r - 1);
             }
         }
         return ans;
