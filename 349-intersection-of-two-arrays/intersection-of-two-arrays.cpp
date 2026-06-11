9f95d4a7da3688b78aec9b4413076eae71350440
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-11 19:04:26 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (82.47%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -7,18 +7,20 @@ public:
         int i = 0, j = 0, n = nums1.size(), m = nums2.size();
         vector<int> ans;
         while(i < n && j < m){
-            if(nums1[i] == nums2[j]){
-                ans.push_back(nums1[i]);
-
-                while(i+1 < n && nums1[i+1] == ans.back()) i++;
-                while(j+1 < m && nums2[j+1] == ans.back()) j++;
-            }
-
             if(nums1[i] < nums2[j]){
                 i++;
             }
-            else{
+            else if(nums1[i] > nums2[j]){
                 j++;
+            }
+
+            else{
+                ans.push_back(nums1[i]);
+                int curr = nums2[j];
+
+                while(i < n && j < m && nums1[i] == curr && nums2[j] == curr){
+                    i++; j++;
+                }
             }
         }
 
