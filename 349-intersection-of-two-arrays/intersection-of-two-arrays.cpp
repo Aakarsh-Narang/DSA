095aa070376c7fdee0ef68a474f2762c4236a9d3
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-11 18:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (74.54%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -8,8 +8,10 @@ public:
         vector<int> ans;
         while(i < n && j < m){
             if(nums1[i] == nums2[j]){
-                if(ans.size() == 0 || ans.back() != nums1[i])
-                    ans.push_back(nums1[i]);
+                ans.push_back(nums1[i]);
+
+                while(i+1 < n && nums1[i+1] == ans.back()) i++;
+                while(j+1 < m && nums2[j+1] == ans.back()) j++;
             }
 
             if(nums1[i] < nums2[j]){
