7e4912748fca28967dd3f72030a9d0c981d32e09
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-13 01:19:57 +0530

    Time: 7 ms (8.91%) | Memory: 13 MB (6.91%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -3,22 +3,21 @@ public:
     int longestValidParentheses(string s) {
         if(s.size() == 0) return 0;
         int n = s.size();
-        stack<int> st;
         vector<int> dp(n, 0);
+        stack<int> st;
 
-        for(int i = 0; i < s.size(); i++){
-            char ch = s[i];
-            if(ch == '(') st.push(i);
+        for(int i = 0; i < n; i++){
+            if(s[i] == '('){
+                st.push(i);
+            }
             else{
-                if(!st.empty()){
-                    int lastIdx = st.top();
-                    st.pop();
-                    dp[i] = (i-lastIdx+1);  
-                    if(lastIdx > 0) dp[i] += dp[lastIdx-1];
-                }
+                if(st.empty()) continue;
+                int lastIdx = st.top();
+                st.pop();
+                dp[i] = (i - lastIdx + 1);
+                if(lastIdx > 0) dp[i] += dp[lastIdx - 1];
             }
         }
-
         return *max_element(dp.begin(), dp.end());
     }
 };
