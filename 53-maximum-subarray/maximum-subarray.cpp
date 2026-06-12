a22b11a2d8f93fe7ef2a167637a5a76362c56ddf
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-12 16:13:18 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (80.03%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSoFar = nums[0], maxSum = nums[0];
+
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] > maxSoFar + nums[i]){
+                maxSoFar = nums[i];
+            }
+            else{
+                maxSoFar += nums[i];
+            }
+
+            maxSum = max(maxSum, maxSoFar);
+        }
+
+        return maxSum;
+    }
+};
