c4a1e72cceb12c3bf2c4fc7a315e8f395b707aa7
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-23 12:52:19 +0530

    Time: 130 ms (12.22%) | Memory: 315 MB (8.89%) - LeetSync

--- 3807-find-maximum-value-in-a-constrained-sequence/find-maximum-value-in-a-constrained-sequence.cpp
+++ 3807-find-maximum-value-in-a-constrained-sequence/find-maximum-value-in-a-constrained-sequence.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int findMaxVal(int n, vector<vector<int>>& restrictions, vector<int>& diff) {
-        vector<int> left(n, 0), right(n, 0);
-        int ans;
+        vector<int> left(n, 0);
 
         unordered_map<int, int> mp;
         for(auto& r: restrictions){
@@ -23,21 +22,15 @@ public:
 
 
         // Right iteration
-        right[n-1] = left[n-1];
         for(int i = n-2; i >= 0; i--){
             if(mp.count(i)){
-                right[i] = min(mp[i], right[i+1] + diff[i]);
+                left[i] = min({mp[i], left[i], left[i+1] + diff[i]});
                 continue;
             }
 
-            right[i] = right[i+1] + diff[i];
+            left[i] = min(left[i], left[i+1] + diff[i]);
         }
 
-        // use min at each index
-        for(int i = 0; i < n; i++){
-            ans = max(ans, min(left[i], right[i]));
-        }
-        
-        return ans;
+        return *max_element(left.begin(), left.end());
     }
 };
