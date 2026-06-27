b97956a198ec16a8cef27c55da8fdb9f37909872
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-27 15:03:56 +0530

    Time: 5 ms (43.42%) | Memory: 9.8 MB (31.52%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -7,10 +7,10 @@ public:
 
         if(dp[i][j] != -1) return dp[i][j] == 1;
 
-        bool takeFrom1, takeFrom2;
-        if(s1[i] == s3[i+j])
+        bool takeFrom1 = false, takeFrom2 = false;
+        if(i < s1.size() && s1[i] == s3[i+j])
             takeFrom1 = solve(s1, s2, s3, i+1, j, dp);
-        if(s2[j] == s3[i+j])
+        if(j < s2.size() && s2[j] == s3[i+j])
             takeFrom2 = solve(s1, s2, s3, i, j+1, dp);
 
         return dp[i][j] = takeFrom1 || takeFrom2;
@@ -20,7 +20,6 @@ public:
         if(s3.size() != m+n) return false;
 
         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        string curr = "";
 
         return solve(s1, s2, s3, 0, 0, dp);
     }
