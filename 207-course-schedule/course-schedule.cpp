7e8f84a51273926a4c0b5df1bf43013f575f9225
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-29 15:58:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (81.26%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites){
+        vector<int> indeg(numCourses, 0), ans;
+        vector<vector<int>> adj(numCourses);
+
+        for(auto& v : prerequisites){
+            adj[v[1]].push_back(v[0]);
+            indeg[v[0]]++;
+        }
+
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++){
+            if(indeg[i] == 0)
+                q.push(i);
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            for(auto& nbr : adj[node]){
+                indeg[nbr]--;
+                if(indeg[nbr] == 0) q.push(nbr);
+            }
+
+            ans.push_back(node);
+        }
+
+        return ans.size() == numCourses;
+    }
+};
