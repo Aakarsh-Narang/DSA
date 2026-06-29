d7c012bc4573c4cdf395df1cf297e67dc3b843d5
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-06-29 17:17:55 +0530

    Time: 35 ms (75.89%) | Memory: 56.6 MB (57.30%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.cpp
+++ 310-minimum-height-trees/minimum-height-trees.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if(n == 2) return {0,1};
+        if(n==1) return {0};
         int cnt = n;
         vector<vector<int>> adj(n);
         vector<int> indeg(n);
         vector<bool> processed(n, 0);
 
-        for(auto& e : edges){
+        for (auto& e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
 
@@ -14,35 +16,47 @@ public:
             indeg[e[1]]++;
         }
 
-        while(cnt > 2){
-            queue<int> q;
-            for(int i = 0; i < n; i++){
-                if(indeg[i] == 1){
-                    q.push(i);
-                    indeg[i]--;
-                    cnt--; // keeps a track of how many nodes with indeg > 1 exist or have not been removed
-                }
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indeg[i] == 1) {
+                q.push(i);
+                indeg[i]--;
+            }
+        }
+        q.push(-1);
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            // cout <<node<<" ";
+
+            if(node == -1){
+                if(cnt <=2) break;
+                q.push(-1);
+                continue;
             }
 
-            while(!q.empty()){
-                int node = q.front();
-                q.pop();
-                processed[node] = 1;
-                // cout <<node<<" ";
-
-                for(auto& nbr : adj[node]){
+            cnt--; // keeps a track of how many nodes with indeg > 1 exist or have not been removed
+            processed[node] = 1;
+           
+            for (auto& nbr : adj[node]) {
+                indeg[nbr]--;
+                if (indeg[nbr] == 1) {
+                    q.push(nbr);
                     indeg[nbr]--;
                 }
             }
         }
 
         vector<int> ans;
-        for(int i = 0; i < n; i++){
-            if(!processed[i]){
+        for(int i = 0; i < n; i++) {
+            if (!processed[i]) {
                 ans.push_back(i);
             }
         }
 
         return ans;
+    
     }
 };
