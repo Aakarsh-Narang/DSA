57aa9172053038faf86ae31bacf1224817a0eb45
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-07-04 01:03:10 +0530

    Time: 294 ms (56.01%) | Memory: 138.6 MB (64.71%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,36 +1,46 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& adj, vector<unordered_set<int>>& ans){
-        for(auto& nbr : adj[node]){
-            if(ans[nbr].empty())
-                dfs(nbr, adj, ans);
-
-            ans[node].insert(ans[nbr].begin(), ans[nbr].end());
-            ans[node].insert(nbr);
-        }
-    }
-
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
-        vector<unordered_set<int>> ans(n);
-        vector<int> indeg(n, 0);
+        vector<int> indegree(n, 0);
 
-        for(auto& e : edges){
-            adj[e[1]].push_back(e[0]);
-            indeg[e[0]]++;
+        for (auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+            indegree[e[1]]++;
         }
 
-        for(int i = 0; i < n; ++i){
-            if(!indeg[i])
-                dfs(i, adj, ans);
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
         }
 
-        vector<vector<int>> res(n);
-        for(int i = 0; i < n; i++){
-            res[i] = vector<int>(ans[i].begin(), ans[i].end());
-            sort(res[i].begin(), res[i].end());
+        vector<unordered_set<int>> anc(n);
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (auto &nbr : adj[node]) {
+
+                // node itself is an ancestor
+                anc[nbr].insert(node);
+
+                // all ancestors of node are also ancestors of nbr
+                anc[nbr].insert(anc[node].begin(), anc[node].end());
+
+                if (--indegree[nbr] == 0)
+                    q.push(nbr);
+            }
         }
 
-        return res;
+        vector<vector<int>> ans(n);
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = vector<int>(anc[i].begin(), anc[i].end());
+            sort(ans[i].begin(), ans[i].end());
+        }
+
+        return ans;
     }
 };
