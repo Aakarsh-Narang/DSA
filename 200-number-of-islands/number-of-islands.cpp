83b73a38941eae077f76576d4c8d24fbcbfad5fc
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-07-06 16:29:39 +0530

    Time: 29 ms (42.05%) | Memory: 23.2 MB (29.37%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
     void expand(vector<vector<char>>& grid, int row, int col){
-        vector<int> dir = {-1, 0, 1, 0 , -1};
+        int m = grid.size(), n = grid[0].size();
+        int dir[] = {-1, 0, 1, 0, -1};
         queue<pair<int, int>> q;
-        q.push({row,col});
+
+        q.push({row, col});
         grid[row][col] = '0';
 
         while(!q.empty()){
-            auto [r, c] = q.front();
+            auto [r,c] = q.front();
             q.pop();
 
             for(int i = 0; i < 4; i++){
-                int nr = r + dir[i];
-                int nc = c + dir[i+1];
+                int nr = r + dir[i], nc = c + dir[i+1];
 
-                if(nr >= 0 && nc >= 0 && nr < grid.size() && nc < grid[0].size() && grid[nr][nc] == '1'){
+                if(nr >= 0 && nr < m && nc >=0 && nc < n && grid[nr][nc] == '1'){
                     grid[nr][nc] = '0';
                     q.push({nr, nc});
                 }
             }
         }
-
     }
-    int numIslands(vector<vector<char>>& grid) {
-        int cnt = 0;
 
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size(), ans = 0;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1'){
-                    cnt++;
+                    ans++;
                     expand(grid, i, j);
                 }
             }
-        }   
+        }
 
-        return cnt;
+        return ans;
     }
 };
