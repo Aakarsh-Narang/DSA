d90f54aa25737ca32a211e4291311aea0f850a0d
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-07-07 12:38:47 +0530

    Time: 1813 ms (5.82%) | Memory: 547 MB (5.05%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums, vector<vector<int>>& queries, int k){
+        int n = nums.size(), currSum = 0;
+        vector<int> diff(n+1, 0);
+
+        // Building diff array as per queries
+        for(int i = 0; i < k; i++){
+            auto q = queries[i];
+            diff[q[0]] += q[2];
+            diff[q[1] + 1] -= q[2];
+        }
+
+        // Updating nums array
+        for(int i = 0; i < n; i++){
+            currSum += diff[i];
+            if(currSum < nums[i]) return false;
+        }
+
+        return true;
+    }
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int lo = 0, hi = queries.size(), ans = -1;
+        while(lo <= hi){
+            int mid = (hi - lo) / 2 + lo;
+            if(isPossible(nums, queries, mid)){
+                ans = mid;
+                hi = mid-1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
