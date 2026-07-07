a89f941336b85c9c971c2fcd7dcf93be6d29f70a
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-07-07 12:22:46 +0530

    Time: 7 ms (63.30%) | Memory: 292.8 MB (67.14%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -4,11 +4,13 @@ public:
         int n = nums.size(), currSum = 0;
         vector<int> diff(n+1, 0);
 
+        // Build diff array
         for(auto& q : queries){
             diff[q[0]] += 1;
             diff[q[1]+1] -= 1;
         }
 
+        // Update nums as per max operation allowed
         for(int i = 0; i < n; i++){
             currSum += diff[i];
             nums[i] = max(0, nums[i] - currSum);
