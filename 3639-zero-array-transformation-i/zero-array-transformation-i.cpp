ef50a1168c24061814bf0ae276ee3e35bdab17f7
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-07-07 12:22:03 +0530

    Time: 4 ms (82.41%) | Memory: 292.9 MB (47.50%) - LeetSync

---
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size(), currSum = 0;
+        vector<int> diff(n+1, 0);
+
+        for(auto& q : queries){
+            diff[q[0]] += 1;
+            diff[q[1]+1] -= 1;
+        }
+
+        for(int i = 0; i < n; i++){
+            currSum += diff[i];
+            nums[i] = max(0, nums[i] - currSum);
+        }
+
+        if(*max_element(nums.begin(), nums.end()) > 0) return false;
+        return true;
+    }
+};
