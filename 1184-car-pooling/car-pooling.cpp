9434177db1eb007e0dc08f3d31d9ae102f6217fb
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-07-07 12:05:23 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (29.44%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         // As per the constraints : 0 <= fromi < toi <= 1000
-        vector<int> times(1001, 0);
+        map<int, int> times;
         int currSum = 0;
 
         for(auto& v : trips){
@@ -11,8 +11,8 @@ public:
             times[to] -= people;
         }
 
-        for(int i = 0; i < 1001; i++){
-            currSum += times[i];
+        for(auto [_, people] : times){
+            currSum += people;
             if(currSum > capacity)
                 return false;
         }
