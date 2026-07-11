d65672ba6dc17743728e7de3a20d9a12ca490528
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-07-11 16:19:12 +0530

    Time: 56 ms (44.13%) | Memory: 136.7 MB (36.55%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
-    void dfs(int node, vector<int>& nodes, vector<vector<int>>& adj, vector<int>& vis){
-        vis[node] = 1;
-        nodes.push_back(node);
-        for(auto& nbr : adj[node]){
-            if(!vis[nbr]){
-                dfs(nbr, nodes, adj, vis);
-            }
-        }
-    }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
 
-        for(auto& e : edges){
+        for (auto &e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
-        
-        vector<int> vis(n, 0);
-        int cnt = 0;
-        
-        for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                vector<int> nodes;
-                bool flg = 1;
 
-                dfs(i, nodes, adj, vis);
+        vector<bool> vis(n, false);
+        int ans = 0;
 
-                int totalNodes = nodes.size();
-                for(auto& n : nodes){
-                    if(adj[n].size() != totalNodes-1) flg = 0;
+        for (int i = 0; i < n; i++) {
+            if (vis[i]) continue;
+
+            queue<int> q;
+            q.push(i);
+            vis[i] = true;
+
+            int nodes = 0;
+            int degreeSum = 0;
+
+            while (!q.empty()) {
+                int node = q.front();
+                q.pop();
+
+                nodes++;
+                degreeSum += adj[node].size();
+
+                for (int nbr : adj[node]) {
+                    if (!vis[nbr]) {
+                        vis[nbr] = true;
+                        q.push(nbr);
+                    }
                 }
-
-                if(flg) cnt++;
             }
+
+            if (degreeSum == nodes * (nodes - 1))
+                ans++;
         }
 
-        return cnt;
+        return ans;
     }
 };
