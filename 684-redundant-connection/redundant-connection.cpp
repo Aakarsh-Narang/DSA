e89e136d89f9814864df6ade9081270e625ae9c3
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-07-12 17:12:37 +0530

    Time: 15 ms (10.49%) | Memory: 23.8 MB (7.03%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -12,6 +12,29 @@ public:
 
         return false;
     }
+    bool bfs(int node, int target, vector<vector<int>>& adj){
+        queue<int> q;
+        vector<bool> vis(adj.size()+1, false);
+
+        q.push(node);
+        vis[node] = 1;
+
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+
+            for(auto& nbr : adj[curr]){
+                if(!vis[nbr]){
+                    vis[nbr] = 1;
+                    q.push(nbr);
+                }
+                if(nbr == target) return true;
+            }
+
+        }
+
+        return false;
+    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = 0; 
         for(auto& e : edges){ 
@@ -25,7 +48,7 @@ public:
 
             vector<bool> vis(n+1, false);
 
-            if (dfs(u, v, adj, vis))
+            if (bfs(u, v, adj))
                 ans = e;
 
             adj[u].push_back(v);
