378ff49eca67c72924283b50e4f0599e76d4f774
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-07-18 13:04:04 +0530

    Time: 22 ms (87.46%) | Memory: 126.3 MB (47.42%) - LeetSync

--- 1286-constrained-subsequence-sum/constrained-subsequence-sum.cpp
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
-        int n = nums.size();
+        int n = nums.size(), ans;
         vector<int> dp(n, 0);
         deque<int> dq;
 
         dp[0]= nums[0];
         dq.push_back(0);
+        ans = dp[0];
 
         for(int i = 1; i < n; i++){
             // Pop all expired ranges
@@ -14,6 +15,7 @@ public:
                 dq.pop_front();
 
             dp[i] = max(nums[i], nums[i] + dp[dq.front()]);  // Kadane's Algo
+            ans = max(ans, dp[i]);
 
             // Pop all smaller Sums from the back
             while(!dq.empty() && dp[dq.back()] < dp[i]) 
@@ -22,6 +24,6 @@ public:
             dq.push_back(i);
         }
 
-        return *max_element(dp.begin(), dp.end());
+        return ans;
     }
 };
