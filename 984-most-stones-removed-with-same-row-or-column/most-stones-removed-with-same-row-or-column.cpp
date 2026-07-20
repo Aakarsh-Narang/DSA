58ae802320e7241ecf6cf2f8c43f9f8e0b5a6438
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-07-21 00:28:41 +0530

    Time: 22 ms (52.41%) | Memory: 33.8 MB (5.10%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    vector<int> parent, size;
+
+    void dsu(int n){
+        parent.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+        size.resize(n);
+    }
+
+    int find(int a){
+        if(parent[a] == a){
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+
+    void unite(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if(rootA == rootB) return;  // Already in same component
+
+        if(size[rootA] < size[rootB]){
+            parent[rootA] = rootB;
+            size[rootB] += size[rootA];
+        }
+        else{
+            parent[rootB] = rootA;
+            size[rootA] += size[rootB];
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        const int OFFSET = 10001;
+        dsu(20002);
+
+        unordered_set<int> nodes;
+
+        for (auto &stone : stones) {
+            int row = stone[0];
+            int col = stone[1] + OFFSET;  // As row & col numbers may overlap
+
+            unite(row, col);
+
+            nodes.insert(row);
+            nodes.insert(col);
+        }
+
+        int components = 0;
+        for(auto& n : nodes){
+            if(parent[n] == n) components++;
+        }
+
+        return stones.size() - components;
+    }
+};
