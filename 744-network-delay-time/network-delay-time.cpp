f625a6874d75225c361ecb71754216d5af822579
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-07-24 14:20:11 +0530

    Time: 81 ms (64.42%) | Memory: 44.2 MB (90.24%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k){
-        vector<int> dist(n+1, 1e7);
-        dist[k] = 0;
-        dist[0] = 0;
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        priority_queue<pair<int, int>> pq;   // {weight, node}
+        vector<vector<pair<int, int>>> adj(n);
+        int ans;
+        vector<int> dist(n,1e7);
 
-        for(int i = 0; i < n; i++){
-            for(auto& e : times){
-                int u = e[0], v = e[1], w = e[2];
-                if(dist[u] + w < dist[v]){
-                    dist[v] = dist[u] + w;
+        for(auto& e : times){
+            e[0]--;
+            e[1]--;
+            adj[e[0]].push_back({e[1], e[2]});
+        }
+        k--;
+        
+        pq.push({0, k});
+        dist[k] = 0;
+
+        while(!pq.empty()){
+            auto [wt, node] = pq.top();
+            pq.pop();
+
+            for(auto& e : adj[node]){
+                auto [nbr, w] = e;
+
+                if(dist[nbr] == -1 || w + wt < dist[nbr]){
+                    dist[nbr] = w + wt;
+                    pq.push({w+wt, nbr});
                 }
             }
-        }
-        if(*max_element(dist.begin(), dist.end()) == 1e7) return -1;
-        return *max_element(dist.begin(), dist.end());
+        }   
+
+        ans = *max_element(dist.begin(), dist.end());
+        if(ans == 1e7) return -1;
+        return ans;
     }
 };
