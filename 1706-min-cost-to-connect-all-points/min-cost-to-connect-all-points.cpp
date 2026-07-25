87ec2355958569c73ed7510b3ce33ce08c064ecf
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-07-25 14:11:21 +0530

    Time: 466 ms (32.64%) | Memory: 110 MB (38.25%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,11 +1,40 @@
 class Solution {
 public:
-    // Prim's Algorithm
+    // Kruskals's Algorithm (DSU)
+
+    vector<int> parent, size;
+
+    void dsu(int n){
+        parent.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+        size.resize(n, 1);
+    }
+
+    int find(int a){
+        if(parent[a] == a) return a;
+
+        return parent[a] = find(parent[a]);
+    }
+
+    void unite(int a, int b){
+        a = find(a);
+        b = find(b);
+
+        if(a == b) return;
+
+        if(size[a] < size[b]){
+            parent[a] = b;
+            size[b] += size[a];
+        }
+        else{
+            parent[b] = a;
+            size[a] += size[b];
+        }
+    }
+
     int minCostConnectPoints(vector<vector<int>>& points) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
         int totalCost = 0, n = points.size();
-        vector<vector<pair<int, int>>> adj(n);
-        vector<int> vis(n, 0);
+        vector<tuple<int, int, int>> edges;
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
@@ -13,26 +42,21 @@ public:
                 int x1 = points[i][0], y1 = points[i][1];
                 int x2 = points[j][0], y2 = points[j][1];
                 int dx = abs(x2 - x1), dy = abs(y2 - y1);
-                adj[i].push_back({j, dx + dy});
+                edges.push_back({dx + dy, i, j});
             }
         }
-
-        pq.push({0, 0});
-
-        while(!pq.empty()){
-            auto [wt, node] = pq.top();
-            pq.pop();
-            if(vis[node]) continue;
         
-            vis[node] = 1;
+        dsu(n);
+        sort(edges.begin(), edges.end());
+
+        for(auto& [wt, a, b] : edges){
+            if(find(a) == find(b)) continue;
+
             totalCost += wt;
 
-            for(auto& [nbr, wt] : adj[node]){
-                if(vis[nbr]) continue;
-                pq.push({wt, nbr});
-            }
+            unite(a, b);
         }
-
+        
         return totalCost;
     }
 };
