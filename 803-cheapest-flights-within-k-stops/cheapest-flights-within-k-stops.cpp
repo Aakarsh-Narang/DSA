04b86ac42b53cd70deebb178dcd8144c9f8b8814
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-08-01 15:19:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (17.04%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>> adj(n);
-        vector<int> dist(n,-1);
-        for (auto& f : flights) {
-            adj[f[0]].push_back({f[1], f[2]});  // Nbr-Cost
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& f : flights) adj[f[0]].push_back({f[1], f[2]});
+
+        // Min-Heap stores: tuple<cost, node, stops_taken>
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+        
+        // Tracks the minimum stops used to reach a node so far
+        vector<int> minStops(n, INT_MAX);
+
+        pq.push({0, src, 0});
+
+        while(!pq.empty()){
+            auto [cost, node, stops] = pq.top();
+            pq.pop();
+
+            if(node == dst) return cost;
+
+            if(minStops[node] > stops)
+                minStops[node] = stops;
+            
+            else continue;
+            
+
+            if(stops > k) continue;
+
+            for(auto& [nbr, wt] : adj[node]){
+                pq.push({cost + wt, nbr, stops + 1});
+            }   
         }
 
-        queue<tuple<int,int,int>> q;    // Node-Cost-Stops
-
-        q.push({src,0,0});  //Src itself has 0 cost nd 0 stops
-        dist[src]=0;
-
-        while(!q.empty()){
-            auto [node, cost, stops]=q.front();
-            q.pop();
-
-            if(stops>k) continue;   // No use cheking further
-
-            for(auto& nbr:adj[node]){
-                int newCost=cost+nbr.second;
-                
-                // Checking if cheaper price is available
-                if(newCost < dist[nbr.first] || dist[nbr.first]==-1){
-                    dist[nbr.first]=newCost;
-                    q.push({nbr.first,newCost,stops+1});
-                }
-                
-            }
-        }
-
-        return dist[dst];
+        return -1;
     }
 };
