e2058906ff55c0c4a1b1d753f26a0bf645a4c1ce
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-08-03 01:01:40 +0530

    Time: 311 ms (5.00%) | Memory: 119.4 MB (5.00%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
+    int play(vector<int>& nums, vector<vector<vector<int>>>& dp, int i, int j, bool isPlayer1){
+        if(i > j) return 0;
+
+        if(dp[i][j][isPlayer1] != -1) return dp[i][j][isPlayer1];
+
+        int takeLeft, takeRight;
+        // Player1 tries to maximise the score
+        if(isPlayer1){
+            takeLeft = nums[i] + play(nums, dp, i+1, j, 0);
+            takeRight = nums[j] + play(nums, dp, i, j-1, 0);
+            return dp[i][j][1] = max(takeLeft, takeRight);
+        }
+        // Player2 tries to minimize the score
+        else{
+            takeLeft = play(nums, dp, i+1, j, 1);
+            takeRight = play(nums, dp, i, j-1, 1);
+            return dp[i][j][0] = min(takeLeft, takeRight);
+        }
+    }
     bool stoneGame(vector<int>& piles) {
-        // Since the number of piles is even, Alice can choose on her first move whether to collect all even-indexed piles or all odd-indexed piles.
-        // She first computes:
-        // - evenSum = sum of stones at even indices
-        // - oddSum = sum of stones at odd indices
-        // Alice chooses the parity with the larger total on her first move and, thereafter, always picks the end corresponding to that parity.
-        // Bob is forced to take piles from the opposite parity.
-        // Since the total number of stones is odd, evenSum ≠ oddSum, so one parity has a strictly larger sum.
-        // Therefore, Alice can always guarantee collecting the larger parity sum and always wins.
-        return 1;
+        int n = piles.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2,-1)));
+
+        int score1 = play(piles, dp, 0, n-1, 1);
+        int total = accumulate(piles.begin(), piles.end(), 0);
+        int score2 = total - score1;
+
+        return score1 >= score2;
     }
 };
