ff093933bfa8c4a20fc57f782eae1a60f299fc18
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-08-03 01:05:41 +0530

    Time: 14 ms (22.56%) | Memory: 19.6 MB (28.59%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    int play(vector<int>& nums, vector<vector<vector<int>>>& dp, int i, int j, bool isPlayer1){
+    int play(vector<int>& nums, vector<vector<int>>& dp, int i, int j){
         if(i > j) return 0;
 
-        if(dp[i][j][isPlayer1] != -1) return dp[i][j][isPlayer1];
+        if(dp[i][j] != -1) return dp[i][j];
 
         int takeLeft, takeRight;
         // Player1 tries to maximise the score
-        if(isPlayer1){
-            takeLeft = nums[i] + play(nums, dp, i+1, j, 0);
-            takeRight = nums[j] + play(nums, dp, i, j-1, 0);
-            return dp[i][j][1] = max(takeLeft, takeRight);
-        }
-        // Player2 tries to minimize the score
-        else{
-            takeLeft = play(nums, dp, i+1, j, 1);
-            takeRight = play(nums, dp, i, j-1, 1);
-            return dp[i][j][0] = min(takeLeft, takeRight);
-        }
+        // After taking left or right, we check both cases if 2nd player took left or right in the next turn and take max of our turns further
+        takeLeft = nums[i] + max(play(nums, dp, i+2, j), play(nums, dp, i+1, j-1));
+        takeRight = nums[j] + max(play(nums, dp, i+1, j-1), play(nums, dp, i, j-2));
+
+        return dp[i][j] = max(takeLeft, takeRight);
     }
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2,-1)));
+        vector<vector<int>> dp(n, vector<int>(n, -1));
 
-        int score1 = play(piles, dp, 0, n-1, 1);
+        int score1 = play(piles, dp, 0, n-1);
         int total = accumulate(piles.begin(), piles.end(), 0);
         int score2 = total - score1;
 
