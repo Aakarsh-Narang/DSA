8033a1307614ac34ab2ea310417cd42098c20b10
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-08-06 16:01:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (55.50%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-        // Tabulation
-    int uniquePathsWithObstacles(vector<vector<int>>& mat) {
-        int rows = mat.size();
-        int cols = mat[0].size();
-        vector<vector<int>> dp(rows, vector<int>(cols, 0));
-        // Mark 1 till no obstacle blocks the straight path
-        for(int i = 0; i < rows; i++){
-            if(mat[i][0] == 1) break;
-            dp[i][0] = 1;
-        }
-        for(int i = 0; i < cols; i++){
-            if(mat[0][i] == 1) break;
-            dp[0][i] = 1;
-        }
-        for (int i = 1; i < rows; i++) {
-            for (int j = 1; j < cols; j++) {
+    int solve(vector<vector<int>>& dp, vector<vector<int>>& mat, int r, int c, int m, int n){
+        if(r < 0 || c < 0) return 0;
 
-                if (mat[i][j] == 1) {
-                    dp[i][j] = 0;
-                }
-                else {
-                    int up = dp[i - 1][j];
-                    int left = dp[i][j - 1];
-                    dp[i][j] = (up + left);
-                }
-            }
-        }
+        if(dp[r][c] != -1) return dp[r][c];
+        if(mat[r][c] == 1) return dp[r][c] = 0;
+        if(r == 0 && c == 0) return dp[r][c] = mat[0][0] != 1;
+        return dp[r][c] = solve(dp, mat, r-1, c, m, n) + solve(dp, mat, r, c-1, m, n);
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
 
-        return dp[rows - 1][cols - 1];
+        return solve(dp, obstacleGrid, m-1, n-1, m, n);
     }
 };
