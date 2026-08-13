2852af4db87722bd4e4a8e6cf59d8dda276a931e
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-08-13 17:49:48 +0530

    Time: 0 ms (100.00%) | Memory: 35.9 MB (81.23%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    struct Compare {
-        bool operator()(pair<int, int> a, pair<int, int> b) {
-            if (a.first == b.first)
-                return a.second > b.second;
-
-            return a.first > b.first;
-        }
-    };
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<int> ans;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;  
+        int n = arr.size(), lo = 0, hi = n - k;
 
-        for(int i=0; i<arr.size(); i++){
-            pq.push({abs(arr[i] - x), arr[i]});
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            // Compare the two elements that would be
+            // excluded if we choose window [mid, mid+k-1]
+            if (x - arr[mid] > arr[mid + k] - x) {
+                // Right side is better -> move window right
+                lo = mid + 1;
+            } 
+            else {
+                // Left side is better (or equal)
+                // Equal => prefer smaller values
+                hi = mid;
+            }
         }
-        for(int i = 0; i < k; i++){
-            ans.push_back(pq.top().second);
-            pq.pop();
-        }        
-        sort(ans.begin(), ans.end());
-        return ans;
+
+        return vector<int>(arr.begin() + lo, arr.begin() + lo + k);
     }
 };
