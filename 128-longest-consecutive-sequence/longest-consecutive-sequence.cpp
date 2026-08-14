ce18b15deaf1041237e6434b3d32c2c4e9efb1d4
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-08-14 15:50:43 +0530

    Time: 84 ms (40.88%) | Memory: 89 MB (49.60%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,17 +4,16 @@ public:
         unordered_set<int> numSet(nums.begin(), nums.end());
         int longest = 0;
 
-        for(auto num : numSet){
-            if(numSet.count(num-1) == 0){
-                int length = 1;
-
-                while(numSet.count(num+length)){
-                    length++;
-                }
-
-                longest = max(longest, length);
+        for(auto n : numSet){
+            if(numSet.count(n-1)) continue;
+            int length = 1;
+            while(numSet.count(n + length) >= 1){
+                length++;
+                // cout << length <<" ";
             }
+            longest = max(longest, length);
         }
+
         return longest;
     }
 };
