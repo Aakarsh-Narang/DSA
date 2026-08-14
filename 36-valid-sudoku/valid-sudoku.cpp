52eb92ab49b2c70311199f7c533348fa4fac40da
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-08-14 11:48:10 +0530

    Time: 8 ms (22.25%) | Memory: 23.2 MB (54.37%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<vector<char>> row(9,vector<char>(9,'0')),col(9,vector<char>(9,'0')),box(9,vector<char>(9,'0'));
+        vector<vector<bool>> row(9,vector<bool>(9, 0)),col(9,vector<bool>(9, 0)),box(9,vector<bool>(9, 0));
         for(int r=0;r<9;r++)
         {
             for(int c=0;c<9;c++)
@@ -10,7 +10,7 @@ public:
                 if(val=='.') 
                     continue;
                 int boxno=3*(r/3)+c/3;
-                if(row[r][val-'1']=='0' && col[c][val-'1']=='0' && box[boxno][val-'1']=='0')
+                if(!row[r][val-'1'] && !col[c][val-'1'] && !box[boxno][val-'1'])
                     row[r][val-'1']=col[c][val-'1']=box[boxno][val-'1']=1;                
                 else
                     return false;
