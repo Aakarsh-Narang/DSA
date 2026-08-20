b13b484e2d9a6d265bcfacd5797270ee402ebe73
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-08-20 14:04:37 +0530

    Time: 16 ms (84.64%) | Memory: 60.8 MB (68.60%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
     // cap -> max amount of money allowed to be robbed
-    bool canRob(vector<int>& nums, int k, int cap){
-        vector<int> dp(nums.size(), 0);
+    bool canRob(vector<int>& nums, int k, int cap) {
+        int count = 0;
 
-        dp[0] = nums[0] <= cap;
-
-        for(int i = 1; i < nums.size(); i++){
-            int notRob = dp[i - 1];
-            int rob = 0;
-
+        for(int i = 0; i < nums.size();) {
             if(nums[i] <= cap) {
-                rob = 1;
-                if(i >= 2)
-                    rob += dp[i - 2];
+                count++;
+                i += 2;   // rob this, so skip adjacent house
+            }
+            else {
+                i++;
             }
 
-            dp[i] = max(rob, notRob);
+            if(count >= k)
+                return true;
         }
 
-        return dp[nums.size()-1] >= k;
+        return false;
     }
-    // bool canRob(vector<int>& nums, int k, int cap) {
-    //     int count = 0;
-
-    //     for(int i = 0; i < nums.size();) {
-    //         if(nums[i] <= cap) {
-    //             count++;
-    //             i += 2;   // rob this, so skip adjacent house
-    //         }
-    //         else {
-    //             i++;
-    //         }
-
-    //         if(count >= k)
-    //             return true;
-    //     }
-
-    //     return false;
-    // }
     int minCapability(vector<int>& nums, int k) {
         int lo = *min_element(nums.begin(), nums.end());
         int hi = *max_element(nums.begin(), nums.end());
