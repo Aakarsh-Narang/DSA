ca16bdc22bd0d4a03ff4b9c05b8494c690c44dfc
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-08-22 16:25:07 +0530

    Time: 71 ms (6.65%) | Memory: 82.8 MB (5.00%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int collect(vector<int>& coins, vector<vector<long long>>& dp, int indx, long long curr, int& amount){
         if(curr == amount) return 0;
-        if(curr > amount || indx == coins.size()) return INT_MAX;
+        if(curr > amount || indx == coins.size()) return 1e7;
 
         if(dp[indx][curr] != -1) return dp[indx][curr];
 
-        long long take = collect(coins, dp, indx, curr+coins[indx], amount);
+        long long take = 1e7;
+        if(coins[indx] <= amount - curr)
+            take = 1 + collect(coins, dp, indx, curr+coins[indx], amount);
         long long skip = collect(coins, dp, indx+1, curr, amount);
-
-        if(take != INT_MAX) take++;
 
         return dp[indx][curr] = min(take, skip);
     }
@@ -19,7 +19,7 @@ public:
 
         int ans = collect(coins, dp, 0, 0, amount);
 
-        if(ans == INT_MAX) return -1;
+        if(ans >= 1e7) return -1;
         return ans; 
     }
 };
