a2eddb3e4f65daba50f4bc5b27fb6edbda7b0d9b
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-08-24 12:08:54 +0530

    Time: 46 ms (15.88%) | Memory: 27.6 MB (32.40%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -4,14 +4,13 @@ public:
         if(i >= text1.size() || j >= text2.size()) return 0;
         if(dp[i][j] != -1) return dp[i][j];
 
-        int take01 = 0, take10= 0, notTake = 0;
+        int take01 = 0, take10= 0;
         if(text1[i] == text2[j]) return dp[i][j] = 1 + solve(text1, text2, dp, i+1, j+1);
 
         take01 = solve(text1, text2, dp, i, j+1);
         take10 = solve(text1, text2, dp, i+1, j);
-        notTake = solve(text1, text2, dp, i+1, j+1);
 
-        return dp[i][j] = max({take10, take01, notTake});
+        return dp[i][j] = max(take10, take01);
     }
     int longestCommonSubsequence(string text1, string text2) {
         int s1 = text1.size(), s2 = text2.size();
