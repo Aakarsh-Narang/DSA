638add1c332e8094b71ea8f4a9e65b98254e6605
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-08-26 14:40:34 +0530

    Time: 10 ms (34.19%) | Memory: 14.3 MB (18.06%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int solve(string& word1, string& word2, int i, int j, vector<vector<int>>& dp){
-        if(i >= word1.size()) return word2.size() - j;
-        if(j >= word2.size()) return word1.size() - i;
+    int solve(string& word1, string& word2, vector<vector<int>>& dp, int i, int j){
+        if(i == word1.size()) return word2.size()-j;
+        if(j == word2.size()) return word1.size()-i;
 
         if(dp[i][j] != -1) return dp[i][j];
-        // Match
-        if(word1[i] == word2[j]){
-            return dp[i][j] = solve(word1, word2, i+1, j+1, dp);
-        }
-        // Mismatch
-        else{
-            // Delete
-            int del = 1 + solve(word1, word2, i+1, j, dp);
-            // Insert
-            int ins = 1 + solve(word1, word2, i, j+1, dp);
-            // Replace
-            int rep = 1 + solve(word1, word2, i+1, j+1, dp);
 
-            return dp[i][j] = min({del, ins, rep});
+        if(word1[i] == word2[j]) return dp[i][j] = solve(word1, word2, dp, i+1, j+1);
+
+        else{
+            // Insert a char
+            int insert = 1 + solve(word1, word2, dp, i, j+1);
+            // Replace a char
+            int replace = 1 + solve(word1, word2, dp, i+1, j+1);
+            // Remove a char
+            int remove = 1 + solve(word1, word2, dp, i+1, j);
+
+            return dp[i][j] = min({insert, replace, remove});
         }
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size(), vector<int>(word2.size(), -1));
+        int n1 = word1.size(), n2 = word2.size();
 
-        return solve(word1, word2, 0, 0, dp);
+        vector<vector<int>> dp(n1, vector<int>(n2, -1));
+
+        return solve(word1, word2, dp, 0, 0);
     }
 };
