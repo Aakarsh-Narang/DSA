0077f84a4a17793c72d7758286e0daa3594dba06
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-09-05 12:19:42 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (97.57%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int fresh = 0, minutes = 0, m = grid.size(), n = grid[0].size();
+        int m = grid.size(), n = grid[0].size(), time = 0, fc = 0;
+        int dir[5] = {-1, 0, 1, 0, -1};
         queue<pair<int, int>> q;
-        vector<int> dir = {-1, 0 , 1, 0, -1};
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 2){
                     q.push({i, j});
                 }
-                else if(grid[i][j] == 1){
-                    fresh++;
+                if(grid[i][j] == 1){
+                    fc++;
                 }
             }
         }
@@ -22,21 +22,23 @@ public:
             q.pop();
 
             if(r == -1 && c == -1){
-                if(!q.empty()) q.push({-1, -1});
-                minutes++;
+                time++;
+                if(q.size()){
+                    q.push({-1, -1});
+                }
                 continue;
             }
+
             for(int i = 0; i < 4; i++){
                 int nr = r + dir[i], nc = c + dir[i+1];
-
                 if(nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1){
-                    q.push({nr, nc});
                     grid[nr][nc] = 2;
-                    fresh--;
+                    q.push({nr, nc});
+                    fc--;
                 }
             }
         }
-        if(fresh == 0) return minutes-1;
-        return -1;
+
+        return fc == 0 ? time-1 : -1;
     }
 };
