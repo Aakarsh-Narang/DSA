1ada162c3e2346320e2e1f7c5b196265bfe123f9
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-09-07 23:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (78.81%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     static constexpr int MOD = 1e9 + 7;
     int distinctSubseqII(string s) {
-        long long tot = 0, prev = 0, dp[26]{};
+        long long tot = 0, prev = 0;
+        vector<long long> dp(26, 0);
 
         for (auto& c : s) {
             c -= 'a';
