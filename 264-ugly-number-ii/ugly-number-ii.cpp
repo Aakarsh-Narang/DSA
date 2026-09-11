92e9cc36c8d01b6aff14a2c352d5bde0d5447e84
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-09-12 02:05:15 +0530

    Time: 74 ms (25.09%) | Memory: 33.4 MB (10.54%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        set<long long> ugly;
-        ugly.insert(1);
-
-        auto it = ugly.begin();
-
-        for(int i = 0; i < n - 1; i++) {
-            long long curr = *it;
-
-            ugly.insert(curr * 2);
-            ugly.insert(curr * 3);
-            ugly.insert(curr * 5);
-
-            it++;
+        vector<int> primes = {2, 3, 5};
+        priority_queue<long, vector<long>, greater<long>> uglyHeap;
+        unordered_set<long> visited;
+        
+        uglyHeap.push(1);
+        visited.insert(1);
+        
+        long curr;
+        for (int i = 0; i < n; ++i) {
+            curr = uglyHeap.top();
+            uglyHeap.pop();
+            for (int prime : primes) {
+                long new_ugly = curr * prime;
+                if (visited.find(new_ugly) == visited.end()) {
+                    uglyHeap.push(new_ugly);
+                    visited.insert(new_ugly);
+                }
+            }
         }
-
-        return *it;
+        return (int)curr;
     }
 };
