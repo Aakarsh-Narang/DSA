38565f0932b4f0ff71016187ee221f5e002f7054
Aakarsh Narang <aakarsh.narang2@gmail.com>
2026-09-12 14:51:58 +0530

    Time: 3 ms (14.90%) | Memory: 8.4 MB (78.99%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int n = s.size();
+        priority_queue<pair<int, char>> pq;
+        vector<int> freq(26, 0);
+        for(auto& ch:s) freq[ch - 'a']++;
+        for(int i=0; i<26; i++){
+            if(!freq[i]) continue;
+            pq.push({freq[i], 'a'+i});
+        }
+        
+        int cnt = pq.top().first;
+        if(cnt > ((n+1)/2)) return "";
+        string ans(n, '-');
+        int indx = 0;
+
+        
+        while(!pq.empty()){
+            auto [cnt1, ch1] = pq.top();
+            pq.pop();
+            ans[indx++] = ch1;
+
+            if(!pq.empty()){
+                auto [cnt2, ch2] = pq.top();
+                pq.pop();
+                ans[indx++] = ch2;
+
+                cnt2--;
+                if(cnt2) pq.push({cnt2, ch2}); 
+            }
+            cnt1--;
+            if(cnt1) pq.push({cnt1, ch1}); 
+        }
+        return ans;
+    }
+};
